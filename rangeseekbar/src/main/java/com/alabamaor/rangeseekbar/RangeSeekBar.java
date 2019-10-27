31aad236b60e08b86b02cef833813b969e720463
Albina Oriyaev <alabama.or@gmail.com>
2019-10-27 22:41:29 +0200

    Adding lines with color. Adding low and high range.

--- rangeseekbar/src/main/java/com/alabamaor/rangeseekbar/RangeSeekBar.java
+++ rangeseekbar/src/main/java/com/alabamaor/rangeseekbar/RangeSeekBar.java
@@ -10,18 +10,32 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.text.DecimalFormat;
+
 
 public class RangeSeekBar extends View {
 
     private static final int DEFAULT_MARGIN = 100;
     private static final int DEFAULT_LINE_SIZE = 20;
     private static final int DEFAULT_INDICATOR_SIZE = 40;
-    private static final float INDICATOR_SPACE = 40;
+    private static final float INDICATOR_SPACE = 0;
+
+
+    private int digitsAfterPoint;
+
+    float lowRange;
+    float highRange;
+
     Context context;
 
     Canvas canvas;
     int screenWidth;
     int screenHeight;
+    Paint indicatorRightPaint;
+    Paint indicatorLeftPaint;
+    Paint linePaint;
+    Paint linePaintRight;
+    Paint linePaintLeft;
     Paint paint;
 
 
@@ -52,6 +66,8 @@ public class RangeSeekBar extends View {
 
     private boolean pressLeftIndicator;
     private boolean pressRightIndicator;
+    private boolean isRightFirst;
+    private float calcedRangeMovement;
 
 
     public RangeSeekBar(Context context, AttributeSet attrs) {
@@ -78,6 +94,11 @@ public class RangeSeekBar extends View {
             marginRight = a.getInt(R.styleable.CustomSeekBar_setMarginRight, DEFAULT_MARGIN);
             marginLeft = a.getInt(R.styleable.CustomSeekBar_setMarginLeft, DEFAULT_MARGIN);
 
+
+            lowRange = a.getFloat(R.styleable.CustomSeekBar_setLowerRange, 0);
+            highRange = a.getFloat(R.styleable.CustomSeekBar_setHighRange, 100);
+            digitsAfterPoint = a.getInt(R.styleable.CustomSeekBar_setDigitsAfterPoint, 1);
+
 //            indicatorRightPositionX = a.getFloat(R.styleable.CustomSeekBar_indicatorRightPositionX, 100);
 //            indicatorRightPositionY = a.getFloat(R.styleable.CustomSeekBar_indicatorRightPositionY, 100);
 //            indicatorLeftPositionX = a.getFloat(R.styleable.CustomSeekBar_indicatorLeftPositionX, 100);
@@ -94,8 +115,24 @@ public class RangeSeekBar extends View {
         pressLeftIndicator = false;
         pressRightIndicator = false;
 
+
+
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.RED);
+
+        indicatorRightPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        indicatorRightPaint.setColor(Color.GREEN);
+
+        indicatorLeftPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        indicatorLeftPaint.setColor(Color.BLUE);
+        linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        linePaint.setColor(Color.RED);
+
+        linePaintRight = new Paint(Paint.ANTI_ALIAS_FLAG);
+        linePaintRight.setColor(Color.YELLOW);
+        linePaintLeft = new Paint(Paint.ANTI_ALIAS_FLAG);
+        linePaintLeft.setColor(Color.GRAY);
+
 
     }
 
@@ -122,6 +159,8 @@ public class RangeSeekBar extends View {
         linePositionStartY = (measureHeight/2) - (lineSize/2);
         linePositionEndX = screenWidth - marginRight;
         linePositionEndY = (measureHeight/2) + (lineSize/2);
+
+        calcedRangeMovement = calcRangeMovement();
     }
 
     @Override
@@ -137,18 +176,29 @@ public class RangeSeekBar extends View {
         RectF rect = new RectF(linePositionStartX, linePositionStartY,
                 linePositionEndX, linePositionEndY);
 
-        canvas.drawRoundRect(rect, 30, 30, paint);
 
-        canvas.drawCircle(indicatorLeftPositionX, rect.centerY(), indicatorLeftRadius, paint);
-        canvas.drawCircle(indicatorRightPositionX, rect.centerY(), indicatorRightRadius, paint);
+        RectF rectRight = new RectF(linePositionStartX, linePositionStartY,
+                indicatorRightPositionX, linePositionEndY);
+
+        RectF rectLeft = new RectF(linePositionStartX, linePositionStartY,
+                indicatorLeftPositionX, linePositionEndY);
+
+        canvas.drawRoundRect(rect, 30, 30, linePaint);
+        canvas.drawRoundRect(rectRight, 30, 30, linePaintRight);
+        canvas.drawRoundRect(rectLeft, 30, 30, linePaintLeft);
 
 
-//        Paint textPaint = new Paint();
-//        textPaint.setColor(Color.WHITE);
-//        textPaint.setStyle(Paint.Style.FILL);
-//
-//        textPaint.setColor(Color.BLACK);
-//        textPaint.setTextSize(24);
+            canvas.drawCircle(indicatorLeftPositionX, rect.centerY(), indicatorLeftRadius, indicatorLeftPaint);
+            canvas.drawCircle(indicatorRightPositionX, rect.centerY(), indicatorRightRadius, indicatorRightPaint);
+
+
+
+        Paint textPaint = new Paint();
+        textPaint.setColor(Color.WHITE);
+        textPaint.setStyle(Paint.Style.FILL);
+
+        textPaint.setColor(Color.BLACK);
+        textPaint.setTextSize(30);
 //
 //
 //        RectF textRect = new RectF(rect.centerX()-150 , rect.centerY() - 140,
@@ -160,8 +210,8 @@ public class RangeSeekBar extends View {
 //        canvas.drawText(rightIndicatorText, (rect.centerX()-130) , rect.centerY() - 100, textPaint);
 //        canvas.drawText(leftIndicatorText, (rect.centerX() +30) , rect.centerY() - 100, textPaint);
 //
-//        canvas.drawText(rightIndicatorText, (rect.centerX()-130) , rect.centerY() + 100, textPaint);
-//        canvas.drawText(leftIndicatorText, (rect.centerX() +30) , rect.centerY() + 100, textPaint);
+        canvas.drawText(leftIndicatorText, (rect.centerX() -130) , rect.centerY() + 100, textPaint);
+        canvas.drawText(rightIndicatorText, (rect.centerX()+30) , rect.centerY() + 100, textPaint);
 
     }
 //
@@ -170,17 +220,17 @@ public class RangeSeekBar extends View {
 
         float x = event.getX();
         float y = event.getY();
-//        event.getAction() == MotionEvent.ACTION_DOWN ||
-
 
         switch (event.getAction()){
 
             case MotionEvent.ACTION_DOWN:
                 if (x >= indicatorLeftPositionX - INDICATOR_SPACE && x < indicatorLeftPositionX+indicatorLeftRadius + INDICATOR_SPACE) {
                     pressLeftIndicator = true;
+                    isRightFirst = false;
                 }
-                if (x >= indicatorRightPositionX - INDICATOR_SPACE && x < indicatorRightPositionX+indicatorRightRadius + INDICATOR_SPACE) {
+                 else if (x >= indicatorRightPositionX - INDICATOR_SPACE && x < indicatorRightPositionX+indicatorRightRadius + INDICATOR_SPACE) {
                     pressRightIndicator = true;
+                    isRightFirst = true;
                 }
                 break;
             case MotionEvent.ACTION_MOVE:
@@ -208,8 +258,8 @@ public class RangeSeekBar extends View {
     public void moveIndicatorLeft(float leftX, int radius)
     {
 
-        if (leftX > indicatorRightPositionX){
-        indicatorLeftPositionX = indicatorRightPositionX;
+        if (leftX > indicatorRightPositionX-indicatorRightRadius/3){
+        indicatorLeftPositionX = indicatorRightPositionX-indicatorRightRadius/3;
     }
         else if (leftX < linePositionStartX){
             indicatorLeftPositionX = marginLeft;
@@ -222,13 +272,14 @@ public class RangeSeekBar extends View {
         }
         indicatorLeftRadius = radius;
 
+        leftIndicatorText  = String.valueOf(getFormatDigitsAfterPoint().format(calcRangeMovement(indicatorLeftPositionX)));
         invalidate();
     }
     public void moveIndicatorRight(float leftX, int radius)
     {
 
-        if (leftX < indicatorLeftPositionX){
-        indicatorRightPositionX = indicatorLeftPositionX;
+        if (leftX < indicatorLeftPositionX + indicatorLeftRadius/3){
+        indicatorRightPositionX = indicatorLeftPositionX + indicatorLeftRadius/3;
     }
         else if (leftX < linePositionStartX){
             indicatorRightPositionX = marginLeft;
@@ -241,7 +292,37 @@ public class RangeSeekBar extends View {
         }
         indicatorRightRadius = radius;
 
+        rightIndicatorText = String.valueOf(getFormatDigitsAfterPoint().format(calcRangeMovement(indicatorRightPositionX)));
         invalidate();
     }
 
+    public float calcRangeMovement(){
+
+        float calcRange = highRange - lowRange;
+        float calcLine = linePositionEndX - linePositionStartX;
+        return calcLine / calcRange;
+    }
+
+    public float calcRangeMovement(float position){
+
+        return (position-marginRight) / calcedRangeMovement;
+    }
+
+    public DecimalFormat getFormatDigitsAfterPoint(){
+        switch (digitsAfterPoint){
+
+            case 0:
+                return new DecimalFormat("##");
+            case 1:
+                return new DecimalFormat("##.#");
+            case 3:
+                return new DecimalFormat("##.###");
+            case 4:
+                return new DecimalFormat("##.####");
+                default:
+                return new DecimalFormat("##.##");
+
+        }
+
+    }
 }
